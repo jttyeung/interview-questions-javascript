66b4463605ad95060868fba0497b4eeba7d41ab1
Joanne Yeung <jttyeung@gmail.com>
2017-08-31 13:05:20 -0700

    create asynchronous http.get request

--- rest_queue/UrlController.js
+++ rest_queue/UrlController.js
@@ -2,6 +2,7 @@ const express = require('express'),
   router = express.Router(),
   bodyParser = require('body-parser'),
   Url = require('./datamodel'),
+  redis = require('redis'),
   http = require('http');
   // downloadHtml = require('./downloadHtml');
 
@@ -11,55 +12,41 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.post('/url', (req, res) => {
 
   let url = req.body.url;
-  console.log(url)
-  let html = 'test';
+  console.log('url: ' + url)
+  let html = '';
 
-  http.get(url, (res) => {
-            res.on('data', (chunk) => {
+  http.get(url, (response) => {
+            response.on('data', (chunk) => {
               html += chunk;
+              console.log('html chunk: ' + html);
             });
-          }).on('error', (err) => {
-            console.log("Got error: " + err.message);
+            response.on('end', () => {
+              Url.create({
+                url: url,
+                html: html
+          //     },
+          //     (err, url) => {
+          //       if (err) return response.sendStatus(500).send('There was an issue saving this URL\'s data in the database.');
+          //       res.sendStatus(200).send(url);
+              })
+            })
+          // }).on('error', (err) => {
+          //   console.log("Got error: " + err.message);
           });
 
-  Url.create({
-    url: 'https://www.google.com',
-    html: html
-    // downloadHtml.getHtml(url)
-    // req.body.html
-  },
-  (err, url) => {
-    if (err) return res.status(500).send('There was an issue saving this URL\'s data in the database.');
-    res.status(200).send(url);
-  });
+  console.log('after html: ' + html)
+
+
 
 });
 
 // Returns a URL data given an ID
 router.get('/urls/:id', (req, res) => {
   Url.findById(req.params.id, (err, urlid) => {
-    if (err) return res.status(500).send('There was an issue finding the id.');
-    if (!urlid) return res.status(404).send('No such id was found.')
+    if (err) return res.sendStatus(500).send('There was an issue finding the id.');
+    if (!urlid) return res.sendStatus(404).send('No such id was found.')
     res.status(200).send(urlid);
   });
 });
 
 module.exports = router;
-
-// let getHtml = (url) => {
-
-//   let options = {
-//     host: 'www.example.com',
-//     port: 80,
-//     path: '/',
-//   };
-
-//   http.get(options, (res) => {
-//     res.on('data', (body) => {
-//       return body;
-//     });
-//   }).on('error', (err) => {
-//     console.log("Got error: " + err.message);
-//   });
-
-// };
